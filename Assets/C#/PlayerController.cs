d169c3afacdd47c9c4e0d9dcb57b7f8e2579cb82
Marcosky1 <144145890+Marcosky1@users.noreply.github.com>
2023-11-14 20:26:03 -0500

    Avance3

--- Assets/C#/PlayerController.cs
+++ Assets/C#/PlayerController.cs
@@ -4,26 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public NodeControl Node;
+    public NodeController Node;
     private Vector2 refVelocity;
-    public float tiempoMove;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float tiempoMove = 0.5f;
+    public float Energy = 100.0f; 
+    public float restTime = 5.0f;
+    private float restTimer = 0.0f;
+    private bool isResting = false;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.SnoothDamp(transform.position,Node.transform.position,ref resVelocity,tiempoMove);
+        if (isResting)
+        {
+            restTimer += Time.deltaTime;
+            if (restTimer >= restTime)
+            {
+                Energy = 100.0f;
+                isResting = false;
+            }
+        }
+        else if (Energy <= 0)
+        {
+            Rest();
+        }
+        else
+        {
+            transform.position = Vector2.SmoothDamp(transform.position, Node.transform.position, ref refVelocity, tiempoMove);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag=="Node")
+        if (collision.gameObject.tag == "Node")
         {
-            Node = collision.gameObject.GetComponent<NodeControl>.GetNextNode();
+            NodeController nextNode = collision.gameObject.GetComponent<NodeController>().GetNextNode(Energy);
+            if (nextNode != null)
+            {
+                Energy -= nextNode.energyCost;
+                Node = nextNode;
+            }
         }
+    }
+
+    void Rest()
+    {
+        isResting = true;
+        restTimer = 0.0f;
     }
 }
